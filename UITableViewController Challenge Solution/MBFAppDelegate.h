e25aa0a69c5f7abfaea1f0ba9874667673a331c6
chongkal <hommequirit.seng@gmail.com>
2014-06-19 20:21:42 +0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/MBFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MBFAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by CHONGKAL SENG on 19.06.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/MBFViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBFViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by CHONGKAL SENG on 19.06.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBFViewController : UIViewController
+
+@end
